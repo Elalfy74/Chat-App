9c0d7142958d38400f67b7be3c01c74c54ec86b0
Elalfy74 <mahmoudelalfy13@gmail.com>
2022-11-21 00:01:29 +0200

    added axios insatance

--- frontend/src/services/chat.ts
+++ frontend/src/services/chat.ts
@@ -1,5 +1,4 @@
-import axios from "axios";
-
+import axios from "../lib/axios";
 import {
   GetChatParams,
   GetMessagesParams,
@@ -7,27 +6,21 @@ import {
 } from "./chat.types";
 
 export const getAllChats = ({ token }: { token: string }) => {
-  return axios.get(`${process.env.REACT_APP_BACKEND_URL!}/chat`, {
+  return axios.get("chat", {
     headers: { Authorization: `Bearer ${token}` },
   });
 };
 
 export const getChat = ({ token, otherUserName }: GetChatParams) => {
-  return axios.get(
-    `${process.env.REACT_APP_BACKEND_URL!}/chat/${otherUserName}`,
-    {
-      headers: { Authorization: `Bearer ${token}` },
-    }
-  );
+  return axios.get(`chat/${otherUserName}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
 };
 
 export const getAllMessages = ({ token, chatId }: GetMessagesParams) => {
-  return axios.get(
-    `${process.env.REACT_APP_BACKEND_URL!}/chat/messages/${chatId}`,
-    {
-      headers: { Authorization: `Bearer ${token}` },
-    }
-  );
+  return axios.get(`chat/messages/${chatId}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
 };
 
 export const sendMessage = ({ token, text, chatId }: SendMessageParams) => {
@@ -40,9 +33,5 @@ export const sendMessage = ({ token, text, chatId }: SendMessageParams) => {
     chatId,
   };
 
-  return axios.post(
-    `${process.env.REACT_APP_BACKEND_URL!}/chat/messages/create-msg`,
-    bodyParameters,
-    config
-  );
+  return axios.post("chat/messages/create-msg", bodyParameters, config);
 };
