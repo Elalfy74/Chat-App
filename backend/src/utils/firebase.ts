b1d7a2b9b9a2d4cf964239bdcdc15a287a1e1964
Elalfy74 <mahmoudelalfy13@gmail.com>
2022-11-20 23:08:23 +0200

    fixed env variables

--- backend/src/utils/firebase.ts
+++ backend/src/utils/firebase.ts
@@ -5,7 +5,7 @@ import config from "config";
 export default admin.initializeApp({
   credential: admin.credential.cert({
     projectId: config.get("projectId"),
-    privateKey: config.get("privateKey"),
+    privateKey: config.get<string>("privateKey").replace(/\\n/gm, "\n"),
     clientEmail: config.get("clientEmail"),
   }),
   storageBucket: config.get("bucketUrl"),
